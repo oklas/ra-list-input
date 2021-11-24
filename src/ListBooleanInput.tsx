4ff6ddf9f379fbb5c3e3f44c9c4b0b2ebf720541
Serguei Okladnikov <oklaspec@gmail.com>
2021-11-24 19:35:27 +0300

    fix: test and check #2

--- src/ListBooleanInput.tsx
+++ src/ListBooleanInput.tsx
@@ -1,43 +1,40 @@
-import React from 'react'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Switch from '@material-ui/core/Switch'
+import * as React from 'react';
+import { default as CircularProgress } from '@material-ui/core/CircularProgress';
+import { default as Switch } from '@material-ui/core/Switch';
 import {
   useRecordContext,
+  Record,
   useResourceContext,
   useNotify,
   useRefresh,
-  useMutation
-} from 'react-admin'
+  useMutation,
+  BooleanFieldProps,
+} from 'react-admin';
 
-const ListBooleanInput = (props) => {
-  const record = useRecordContext(props)
+const ListBooleanInput: React.FC<BooleanFieldProps> = (props) => {
+  const record = useRecordContext(props);
   const resource = useResourceContext(props);
-  if(!record) return null
-  const refresh = useRefresh()
-  const notify = useNotify()
-  const {source} = props
-  const {id, [source]: value} = record
-  const [save, {loading}] = useMutation(
+  if (!record) return null;
+  const refresh = useRefresh();
+  const notify = useNotify();
+  const { source }: { source?: string } = props;
+  const { id, [source || '']: value }: Record = record;
+  const [save, { loading }] = useMutation(
     {
-        type: 'update',
-        resource,
-        payload: {data: { id, [source]: !value } },
+      type: 'update',
+      resource,
+      payload: { data: { id, [source || '']: !value } },
     },
     {
       mutationMode: 'undoable',
       onSuccess: refresh,
-      onFailure: error => notify(`Error: ${error.message}`, 'warning'),
+      onFailure: (error) => notify(`Error: ${error.message}`, 'warning'),
     }
-  ) 
+  );
 
-  if(loading) return <CircularProgress />
+  if (loading) return <CircularProgress />;
 
-  return (
-    <Switch
-      checked={value}
-      onChange={save}
-    />
-  )
-}
+  return <Switch checked={value} onChange={(...a: any) => save(...a)} />;
+};
 
-export default ListBooleanInput
+export default ListBooleanInput;
